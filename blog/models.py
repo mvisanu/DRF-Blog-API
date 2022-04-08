af27cac12f341aaa41563e000847363da6bd05e7
Visanu <mvisanu@gmail.com>
2022-04-07 20:07:55 -0500

    Add register for adding user

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.conf import settings
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
